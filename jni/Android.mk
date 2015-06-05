80306bfaa39e63d9be5b8611a74a5ff7f96b1fdc
mobileliker <mobileliker@gmail.com>
2015-06-05 13:24:39 +0800

    本地化配置

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 # OpenCV
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
-include /Users/LL/Desktop/ANDROID_SPACE/sdk/native/jni/OpenCV.mk  
+include D:/GreenProgram/OpenCV-2.4.10-android-sdk/sdk/native/jni/OpenCV.mk
 FILE_LIST := $(wildcard $(LOCAL_PATH)/core/*.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
